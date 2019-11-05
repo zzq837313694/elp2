b34e96bfba8a6c98a23a1642ee213dc2dfbea864
juice-xy <709691703@qq.com>
2019-11-05 09:50:10 +0800

    lh

--- src/main/java/com/cn/elp/util/PageSurpport.java
+++ src/main/java/com/cn/elp/util/PageSurpport.java
@@ -1,4 +1,52 @@
 package com.cn.elp.util;
 
-public class PageSurpport {
+import java.util.List;
+
+public class PageSurpport<T> {
+    private int pageIndex;//当前页
+    private int pageCount;//总页数
+    private int pageSize;//页大小
+    private int totalCount;//总记录数
+    private List<T> dataList;
+
+    public int getPageIndex() {
+        return pageIndex;
+    }
+
+    public void setPageIndex(int pageIndex) {
+        this.pageIndex = pageIndex;
+    }
+
+    public int getPageCount() {
+        if(totalCount%pageSize==0){
+            pageCount=totalCount/pageSize;
+        }else {
+            pageCount=totalCount/pageSize+1;
+        }
+        return pageCount;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public void setTotalCount(int totalCount) {
+        this.totalCount = totalCount;
+    }
+
+    public List<T> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(List<T> dataList) {
+        this.dataList = dataList;
+    }
 }
