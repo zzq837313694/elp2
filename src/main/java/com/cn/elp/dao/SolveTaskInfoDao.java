0e42ff5461c31a92099b976a9244e28214e121e3
jiaosun <2460641706@qq.com>
2019-11-06 15:36:19 +0800

    jyz

---
+++ src/main/java/com/cn/elp/dao/SolveTaskInfoDao.java
@@ -0,0 +1,11 @@
+package com.cn.elp.dao;
+
+import com.cn.elp.POJO.Solvetaskinfo;
+
+import java.util.List;
+
+public interface SolveTaskInfoDao {
+
+        List<Solvetaskinfo> findAllSolveTaskInfo();
+
+}
