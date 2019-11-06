95b8dabcfe9e4c91ac9eb7fecf13ecea5a6a62ef
zzq837313694 <837313694@qq.com>
2019-11-06 19:56:07 +0800

    zzq

--- src/main/java/com/cn/elp/dao/ChecktaskDao.java
+++ src/main/java/com/cn/elp/dao/ChecktaskDao.java
@@ -1,4 +1,9 @@
 package com.cn.elp.dao;
 
+import com.cn.elp.POJO.Checktaskinfo;
+import com.cn.elp.util.ChecktaskCondition;
+import com.cn.elp.util.PageSurpport;
+
 public interface ChecktaskDao {
+    PageSurpport<Checktaskinfo> SelectChecktaskByParam(ChecktaskCondition checktaskCondition);
 }
