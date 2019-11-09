89e77f3a8391558572eb54c035900c5433ce6652
zzq837313694 <837313694@qq.com>
2019-11-09 15:53:23 +0800

    zzq

--- src/main/java/com/cn/elp/util/ChecktaskCondition.java
+++ src/main/java/com/cn/elp/util/ChecktaskCondition.java
@@ -1,11 +1,17 @@
 package com.cn.elp.util;
 
 import com.cn.elp.POJO.Checktaskinfo;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 public class ChecktaskCondition extends Checktaskinfo {
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
     private Date CreateDateMin;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
     private Date CreateDateMax;
     private PageSurpport<Checktaskinfo> pageSurpport;
     private String checkByName;
