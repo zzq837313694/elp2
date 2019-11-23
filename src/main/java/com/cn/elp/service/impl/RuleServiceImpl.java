c78bd4f40b909902d51ad36e285fe38431157b9f
还好 <1192919554@qq.com>
2019-11-23 17:28:57 +0800

    Merge branch 'lcx' of https://github.com/zzq837313694/elp into lcx
    # Conflicts:
    #	src/main/resources/application.properties
    #	src/main/resources/templates/permissionSetting.html

--- src/main/java/com/cn/elp/service/impl/RuleServiceImpl.java
+++ src/main/java/com/cn/elp/service/impl/RuleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cn.elp.service.impl;
 
+import com.cn.elp.POJO.Role;
 import com.cn.elp.POJO.Rule;
+import com.cn.elp.dao.RoleDao;
 import com.cn.elp.dao.RuleDao;
 import com.cn.elp.service.RuleService;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,8 @@ import java.util.List;
 public class RuleServiceImpl implements RuleService {
     @Resource
     RuleDao ruleDao;
+    @Resource
+    RoleDao roleDao;
 
     @Override
     public List<Rule> findAllRuleByPerRuleId(int perRuleId) {
@@ -26,5 +30,35 @@ public class RuleServiceImpl implements RuleService {
     @Override
     public Rule findRuleByRuleId(int ruleId) {
         return ruleDao.findRuleByRuleId(ruleId);
+    }
+
+    @Override
+    public int addRule(Rule rule) {
+        return ruleDao.addRule(rule);
+    }
+
+    @Override
+    public int updateRule(Rule rule) {
+        return ruleDao.updateRule(rule);
+    }
+
+    @Override
+    public int delRule(int ruleId) {
+        int rel=-1;//删除失败
+        List<Role> roleList = roleDao.findAllRole();
+        for(Role role:roleList){
+            String[] ruleList=role.getRuleList().split("-");
+            for (String str:ruleList){
+                int rule = Integer.parseInt(str);
+                if(rule==ruleId){
+                    rel=0;//权限被占用，不能删除
+                    return rel;
+                }
+            }
+        }
+        if(rel==-1){
+            rel=ruleDao.delRule(ruleId);
+        }
+        return rel;
     }
 }
